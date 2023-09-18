b687b27ffb84bedf3466ab679112d698a5e0774c
Rohit Sanwariya <rohitsanwariya1995@gmail.com>
2023-09-18 20:30:06 +0530

    firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOq8Q5XorcxrPteaIWCTBHci_EFXXBZCQ",
+  authDomain: "rn-signal-clone-4966c.firebaseapp.com",
+  projectId: "rn-signal-clone-4966c",
+  storageBucket: "rn-signal-clone-4966c.appspot.com",
+  messagingSenderId: "702870129528",
+  appId: "1:702870129528:web:1a0e9d1056f0c2ba00d68d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
